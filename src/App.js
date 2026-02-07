1d5dd8219d6a792a3e183b42f3ea4df35bece38d
jorge <jorgegusarruda@gmail.com>
2026-02-07 00:52:04 -0300

    correcion en App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           nombre="Shawn Wang"
           pais="Singapur"
           imagen="Shawn"
-          cargo="Singapur"
+          cargo="Ingeniero de Software"
           empresa="Amazon"
           testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
         />
